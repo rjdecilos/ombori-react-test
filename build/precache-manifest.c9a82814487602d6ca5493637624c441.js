065f3e9ce064206076fbccf3692c56e96afc305a
rjdecilos <decilos_rj@yahoo.com>
2019-08-18 05:54:43 -0500

    add build

---
+++ build/precache-manifest.c9a82814487602d6ca5493637624c441.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "470f51c0f7c869e132fa",
+    "url": "/static/js/runtime~main.e089db1f.js"
+  },
+  {
+    "revision": "3c3422b29bda100d7d5b",
+    "url": "/static/js/main.d2e28b4c.chunk.js"
+  },
+  {
+    "revision": "d00053711e6a38943634",
+    "url": "/static/js/3.94c2945d.chunk.js"
+  },
+  {
+    "revision": "5572256f6a9995ddf9c1",
+    "url": "/static/js/2.3b4a378b.chunk.js"
+  },
+  {
+    "revision": "6fe9ada09913243e3002aba1a0419314",
+    "url": "/index.html"
+  }
+];
